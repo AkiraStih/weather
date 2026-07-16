349d21878310a7209e8cd977cabfaf332025cd55
Bogdan <your_email@example.com>
2026-07-17 01:32:41 +0300

    update weather forecats

--- src/api/weather.client.ts
+++ src/api/weather.client.ts
@@ -1,14 +1,14 @@
-import axios from 'axios';
-import { realtimeWeatherSchema, type RealtimeWeather } from './weather.schema';
+import axios from "axios";
+import { forecastWeatherSchema, type ForecastWeather } from "./weather.schema";
 
 const api = axios.create({
-  baseURL: 'https://api.weatherapi.com/v1',
-  params: {
-    key: import.meta.env.VITE_WEATHER_API_KEY,
-  },
+  baseURL: "https://api.weatherapi.com/v1",
+  params: { key: import.meta.env.VITE_WEATHER_API_KEY },
 });
 
-export async function getRealtimeWeather(q: string): Promise<RealtimeWeather> {
-  const { data } = await api.get('/current.json', { params: { q } });
-  return realtimeWeatherSchema.parse(data);
+export async function getWeatherByQuery(q: string): Promise<ForecastWeather> {
+  const { data } = await api.get("/forecast.json", {
+    params: { q, days: 10, aqi: "no", alerts: "no" },
+  });
+  return forecastWeatherSchema.parse(data);
 }
